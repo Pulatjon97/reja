62540bea88c87ea782c9f6ddb3210ca97635c817
John <polatsultonov97@gmail.com>
2026-08-30 17:21:06 +0900

    task fayli qo'shildi

---
+++ task.js
@@ -0,0 +1,13 @@
+function countLetter(letter, word) {
+  let count = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log("Aniqlangan harflar soni:", countLetter("a", "Alloh yetishtirmaydigan narsani havas qildirmaydi!"));
