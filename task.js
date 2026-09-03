370d3e7e3c2c2c61335f8d78a066dc8f9dc571e2
John <polatsultonov97@gmail.com>
2026-09-03 20:56:27 +0900

    TASK - C is added

--- task.js
+++ task.js
@@ -10,7 +10,39 @@ function countLetter(letter, word) {
   return count;
 }
 
-console.log("Aniqlangan harflar soni:", countLetter("a", "Alloh yetishtirmaydigan narsani havas qildirmaydi!"));
+console.log(
+  "Aniqlangan harflar soni:",
+  countLetter("a", "Alloh yetishtirmaydigan narsani havas qildirmaydi!"),
+);
 
+// TASK - C
 
-//Nima gaaaaaap!? 
+function checkContent(str1, str2) {
+  // Uzunliklari teng bo'lmasa, harflar soni ham teng bo'lolmaydi
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const count1 = {};
+  const count2 = {};
+
+  for (const ch of str1) {
+    count1[ch] = (count1[ch] || 0) + 1;
+  }
+
+  for (const ch of str2) {
+    count2[ch] = (count2[ch] || 0) + 1;
+  }
+
+  for (const key in count1) {
+    if (count1[key] !== count2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Misollar:
+console.log(checkContent("mitgroup", "gmtiprou")); // true
+console.log(checkContent("hello", "world")); // false
