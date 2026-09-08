13242875b46e9287708dffcbc88455d77b236f69
John <polatsultonov97@gmail.com>
2026-09-09 00:12:48 +0900

    feat: Task E added

--- task.js
+++ task.js
@@ -1,63 +1,81 @@
-// Task - D
-// npm run task.         <<=========================
+// Task - E
 
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.mahsulotlar = {
-      non: non,
-      lagmon: lagmon,
-      cola: cola,
-    };
-  }
+function getReverse(str) {
+  let reversed = str.split("").reverse().join("");
 
-  // Vaqtni "HH:MM" formatida qaytaradi
-  hozirgiVaqt() {
-    const now = new Date();
-    const soat = now.getHours().toString().padStart(2, "0");
-    const daqiqa = now.getMinutes().toString().padStart(2, "0");
-    return `${soat}:${daqiqa}`;
-  }
+  reversed =
+    reversed.charAt(0).toUpperCase() +
+    reversed.slice(1, -1) +
+    reversed.charAt(reversed.length - 1).toLowerCase();
 
-  // 1) Qoldiqni ko'rsatish
-  qoldiq() {
-    const vaqt = this.hozirgiVaqt();
-    const xabar = `Hozir ${vaqt}da ${this.mahsulotlar.non}ta non, ${this.mahsulotlar.lagmon}ta lagmon va ${this.mahsulotlar.cola}ta cola mavjud!`;
-    console.log(xabar);
-    return xabar;
-  }
-
-  // 2) Sotish - mahsulot miqdorini kamaytiradi
-  sotish(mahsulot, miqdor) {
-    if (this.mahsulotlar[mahsulot] === undefined) {
-      console.log(`Xatolik: "${mahsulot}" degan mahsulot mavjud emas!`);
-      return;
-    }
-    if (this.mahsulotlar[mahsulot] < miqdor) {
-      console.log(`Xatolik: yetarli "${mahsulot}" mavjud emas!`);
-      return;
-    }
-    this.mahsulotlar[mahsulot] -= miqdor;
-    const vaqt = this.hozirgiVaqt();
-    console.log(`Hozir ${vaqt}da ${miqdor}ta ${mahsulot} sotildi!`);
-  }
-
-  // 3) Qabul qilish - mahsulot miqdorini oshiradi
-  qabul(mahsulot, miqdor) {
-    if (this.mahsulotlar[mahsulot] === undefined) {
-      this.mahsulotlar[mahsulot] = 0;
-    }
-    this.mahsulotlar[mahsulot] += miqdor;
-    const vaqt = this.hozirgiVaqt();
-    console.log(`Hozir ${vaqt}da ${miqdor}ta ${mahsulot} qabul qilindi!`);
-  }
+  return reversed + "😂";
 }
 
-// Test qilish:
-const shop = new Shop(4, 5, 2);
-shop.qoldiq();        // Hozir HH:MMda 4ta non, 5ta lagmon va 2ta cola mavjud!
-shop.sotish("non", 3);
-shop.qabul("cola", 4);
-shop.qoldiq();        // Hozir HH:MMda 1ta non, 5ta lagmon va 6ta cola mavjud!
+console.log(getReverse("Samad")); // Damas😂
+
+
+
+
+// // Task - D
+// // npm run task.         <<=========================
+
+// class Shop {
+//   constructor(non, lagmon, cola) {
+//     this.mahsulotlar = {
+//       non: non,
+//       lagmon: lagmon,
+//       cola: cola,
+//     };
+//   }
+
+//   // Vaqtni "HH:MM" formatida qaytaradi
+//   hozirgiVaqt() {
+//     const now = new Date();
+//     const soat = now.getHours().toString().padStart(2, "0");
+//     const daqiqa = now.getMinutes().toString().padStart(2, "0");
+//     return `${soat}:${daqiqa}`;
+//   }
+
+//   // 1) Qoldiqni ko'rsatish
+//   qoldiq() {
+//     const vaqt = this.hozirgiVaqt();
+//     const xabar = `Hozir ${vaqt}da ${this.mahsulotlar.non}ta non, ${this.mahsulotlar.lagmon}ta lagmon va ${this.mahsulotlar.cola}ta cola mavjud!`;
+//     console.log(xabar);
+//     return xabar;
+//   }
+
+//   // 2) Sotish - mahsulot miqdorini kamaytiradi
+//   sotish(mahsulot, miqdor) {
+//     if (this.mahsulotlar[mahsulot] === undefined) {
+//       console.log(`Xatolik: "${mahsulot}" degan mahsulot mavjud emas!`);
+//       return;
+//     }
+//     if (this.mahsulotlar[mahsulot] < miqdor) {
+//       console.log(`Xatolik: yetarli "${mahsulot}" mavjud emas!`);
+//       return;
+//     }
+//     this.mahsulotlar[mahsulot] -= miqdor;
+//     const vaqt = this.hozirgiVaqt();
+//     console.log(`Hozir ${vaqt}da ${miqdor}ta ${mahsulot} sotildi!`);
+//   }
+
+//   // 3) Qabul qilish - mahsulot miqdorini oshiradi
+//   qabul(mahsulot, miqdor) {
+//     if (this.mahsulotlar[mahsulot] === undefined) {
+//       this.mahsulotlar[mahsulot] = 0;
+//     }
+//     this.mahsulotlar[mahsulot] += miqdor;
+//     const vaqt = this.hozirgiVaqt();
+//     console.log(`Hozir ${vaqt}da ${miqdor}ta ${mahsulot} qabul qilindi!`);
+//   }
+// }
+
+// // Test qilish:
+// const shop = new Shop(4, 5, 2);
+// shop.qoldiq();        // Hozir HH:MMda 4ta non, 5ta lagmon va 2ta cola mavjud!
+// shop.sotish("non", 3);
+// shop.qabul("cola", 4);
+// shop.qoldiq();        // Hozir HH:MMda 1ta non, 5ta lagmon va 6ta cola mavjud!
 
 
 
